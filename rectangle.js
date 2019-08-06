ce2ca1033be82a9e29bbe3939345643238183052
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-06 14:03:38 +0700

    up file area of rectangle

---
+++ rectangle.js
@@ -0,0 +1,6 @@
+function area() {
+    let num1 = Number(prompt("Please import length"));
+    let num2 = Number(prompt("Please import width"));
+    let area = num1 * num2;
+    alert('Area of rectangle is ' + area);
+}
