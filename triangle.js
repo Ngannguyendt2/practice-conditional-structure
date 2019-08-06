8cb3ff9a632aa059f30912648d6042a5804bdd51
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-06 14:12:13 +0700

    up file area of triangle

---
+++ triangle.js
@@ -0,0 +1,7 @@
+function areaTriangle() {
+    let num1=Number(prompt("Please import the first edge length"));
+    let num2=Number(prompt("Please import the second edge length"));
+    let area=(num1*num2)/2;
+    alert('Area of Triangle is '+ area);
+
+}
